7bff74366a07236f11916e21b2b80f87b7b6a73a
zhateyev <zhateyev@greet-go.com>
2020-03-23 17:42:06 +0600

    Add RecipeBootstrap

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,12 +17,14 @@ public class Recipe {
   private Integer servings;
   private String source;
   private String url;
+
+  @Lob
   private String directions;
   //todo add
   //private Difficulty difficulty;
 
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-  private Set<Ingredient> ingredients;
+  private Set<Ingredient> ingredients = new HashSet<>();
 
   @Lob
   private Byte[] image;
@@ -36,7 +39,7 @@ public class Recipe {
   @JoinTable(name = "recipe_category",
     joinColumns = @JoinColumn(name = "recipe_id"),
     inverseJoinColumns = @JoinColumn(name = "category_id"))
-  private Set<Category> categories;
+  private Set<Category> categories = new HashSet<>();
 
   public Long getId() {
     return id;
